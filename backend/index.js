321b0b392c25617d52111a81cdf39eff219a34ea
piotr-major-rmtcfm-com <80816815+piotr-major-rmtcfm-com@users.noreply.github.com>
2021-03-22 11:39:49 +0100

    Added BodyParser (#2)
    * Added BodyParser
    
    * used body parser
    
    * added prettierrc.js
    
    * fixed problem with formats
    
    * added return to response

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,17 @@
 const express = require('express');
-const helmet = require('helmet'); 
+const helmet = require('helmet');
 const cors = require('cors');
-const config = require('./config'); 
+const config = require('./config');
 
 const app = express();
 
-app.use(helmet()); 
+app.use(helmet());
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded());
 
-app.get('/', (req, res) => { 
-    res.send("ERP Backend") 
-}); 
+app.get('/', (req, res) => {
+    return res.send('ERP Backend');
+});
 
 app.listen(config.app.port);
