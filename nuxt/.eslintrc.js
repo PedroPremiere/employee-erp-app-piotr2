e5718adafa16e9897176f73d862ac51b77f72e67
piotr-major-rmtcfm-com <80816815+piotr-major-rmtcfm-com@users.noreply.github.com>
2021-06-14 10:13:25 +0200

    added nuxt (#51)
    * added nuxt
    
    * added axios and auth-nuxt
    
    * added config files and env
    
    * changed nuxt.config
    
    * fixed
    
    * removed package-lock.json
    
    * fixed

---
+++ nuxt/.eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+    extends: ['eslint:recommended', 'plugin:vue/recommended'],
+    rules: {
+        'no-unused-vars': [
+            'error',
+            { vars: 'all', args: 'after-used', ignoreRestSiblings: true }
+        ],
+        'vue/html-indent': ['error', 4, { baseIndent: 1 }],
+        'vue/max-attributes-per-line': ['off'],
+        'vue/html-self-closing': ['off'],
+        'vue/singleline-html-element-content-newline': 'off',
+        'vue/multiline-html-element-content-newline': 'off'
+    },
+    parserOptions: {
+        sourceType: 'script'
+    },
+    overrides: [
+        {
+            files: ['src/**/*'],
+            parserOptions: {
+                parser: 'babel-eslint',
+                sourceType: 'module'
+            },
+            env: {
+                browser: true
+            }
+        }
+    ],
+    globals: {
+        $: true,
+        require: true,
+        process: true,
+        module: true
+    }
+};
